161a27d9ea2fd8c9551ac1269ca7c807c6b33571
Cameron <cameron-a-1997@hotmail.com>
2017-04-05 02:49:47 +1200

    Push with new Main

---
+++ src/Main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func pathExtraction() {}	
+func cityExtraction() {}	
+func startCity() {}	
+func pathDecision() {}  
+func exitDecision() {}
+func pathReverse() {}	
+
+# IF for the counter for additional text output (Handled in Main)
+
+func main() {
+	
+}
+
