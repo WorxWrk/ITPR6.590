17ea5c0199c3dc9fd06a18f50a2ef86ef001d709
Cameron <cameron-a-1997@hotmail.com>
2017-05-24 02:45:26 +1200

    Backbone created, inclueding the 2-D arrays, Master Aray and some functonality with printing infomation and moving between 'Cities'. Has a Compiling error, problem is likely curly braces in the simulation function

--- Main.go
+++ Main.go
@@ -1,64 +1,210 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strconv"
+	//"bufio"
+	//"os"
 )
 
-func pathExtraction() {
+func pathExtraction( num int) {
 
 }
-func cityExtraction() {
+func cityExtraction(num int) {
 
 }
 func startCity() {
 
 }
-func pathDecision() {
 
-}
-func exitDecision() {
-
-}
 func pathReverse() {
 
 }
+
+func printer( cityPar string) {
+
+	fmt.Println ("")
+}
+
+func Drive () {
+
+}
+
+
+
+/*
+Function simulation
+Is a single driver run
+
+*/
+func simulation (int driverNumber) {
+	//------------------- 1. simulation initialization
+	var key bool = false
+
+	// new rnd number
+	var currentCity int = 0 // rnd
+
+	var location = allCities[currentCity]  // variable that holds a single city section/quadrant
+	//------------------ 2.
+
+	for key == false{  //Main loop for a single
+
+		var akinaCount int = 0
+
+		// new RND number
+		var pathSelection int = 0 //rnd
+
+		var currentCityName string = location[1][0]
+
+		if nextCityName == "Akina" {
+		akinaCount++
+		}
+
+		//------------------- 3. City movement
+		if pathSelection == 1 { // right side path, int goes up
+			if currentCity == 3 { // restricts int size
+				currentCity = 0
+			}else{
+				currentCity = currentCity + 1
+			}
+			location = allCities[currentCity]
+			var pathName string = location[3][1]
+			var exitName string = location[3][0]
+		}
+
+		if pathSelection == 0{ // left side path, int goes down
+			if currentCity == 0 { //restricts int size
+				currentCity = 3
+			}else {
+				currentCity = currentCity - 1
+			}
+			location = allCities[currentCity]
+			var pathName string = location[0][1]
+			var exitName string = location[0][0]
+		}
+
+		var nextCityName string = location[1][0]
+
+
+		//-------------------------- 4. Extras
+		// Moved to the Top of the Loop to catch the starting City
+		//-------------------------- 5. Exitting and Printing
+
+		// new rnd
+		var exitSelector int = 0 //RND
+
+		if exitSelector == 2 { //  number that allows the driver to exit
+			// related Printing Info
+			key = true
+		}else{     // Exiting failed
+			     // related Printing info
+		}
+	//------------ Printing Extras
+
+	fmt.Println("Driver", driverNumber, "Met With Jhon Jamieson", akinaCount, " Times!")
+
+	if akinaCount != 1 || 2{
+		if akinaCount == 0{
+			fmt.Println("That Driver Missed Out!")
+		}
+		if akinaCount <= 3{
+			fmt.Println("That Driver Needed Lots Of Help!")
+		}
+	}
+
+}
+//
 
 // IF for the counter for additional text output (Handled in Main)
 
 func main() {
 
-	var input string
+	var City1 [3][2] string
+	City1 [0][0] = "Exit 1"
+	City1 [0][1] = "Path 1"
 
+	City1 [1][0] = "City"
+	City1 [1][1] = ""
+
+	City1 [2][0] = "Exit 2"
+	City1 [2][1] = "Path2"
+
+	var City2 [3][2] string
+	City2 [0][0] = "Exit 2"
+	City2 [0][1] = "Path 2"
+
+	City2 [1][0] = "City1"
+	City2 [1][1] = ""
+
+	City2 [2][0] = "Exit 3"
+	City2 [2][1] = "Path 3"
+
+	var City3 [3][2] string
+	City3 [0][0] = "Exit 3"
+	City3 [0][1] = "Path 3"
+
+	City3 [1][0] = "City2"
+	City3 [1][1] = ""
+
+	City3 [2][0] = "Exit 4"
+	City3 [2][1] = "Path 4"
+
+	var City4 [3][2] string
+	City4 [0][0] = "Exit 4"
+	City4 [0][1] = "Path 4"
+
+	City4 [1][0] = "City3"
+	City4 [1][1] = ""
+
+	City4 [2][0] = "Exit 1"
+	City4 [2][1] = "Path 1"
+
+
+var allCities [4] [3][2]string
+																//array for holding all city sections/quadrants
+allCities[0] = City1
+allCities[1] = City2
+allCities[2] = City3
+allCities[3] = City4
+
+
+
+var input int = 0 // if statments for managing the -numbers and over 3 (4+),  over 3 will rest to 0,  - numbers (under 0) will increase to 3
+var input2 int = 1 // 2 possiblities, need an if for number changeing, eg 0 =0 and 1 = 2, this is due to the city name being inbetween the 2 path groups
+var section = complie[input] // variable that holds a single city section/quadrant
+
+var cityname string = section[1][0]
+var pathname string = section[2][1]
+
+if input2 == 1 { // will need to keep the varialbes above for handling the names as the "section" state will change without the statement
+	section = complie[input + input2]  // right side path, int goes up
+	fmt.Println("The Driver is headed to",section[1][0],"from", cityname, "using the path", pathname)
+}else{
+	section = complie[input - input2]  // left side path, int goes down
+}
+
+	//var input string
+/*
 	scanner := bufio.NewScanner(os.Stdin)
 
 	fmt.Println("Enter Path Input: ")
 	scanner.Scan()
 	input = scanner.Text()
-	PathInt := strconv.ParseInt(input, 10, 32)
 
 	fmt.Println("Enter City Input: ")
 	scanner.Scan()
 	input = scanner.Text()
-	CityInt := strconv.ParseInt(input, 10, 32)
+*/
 
-	var path [4]string
-	path[0] = "Path 1 "
-	path[1] = "Path 2"
-	path[2] = "Path 3"
-	path[3] = "Path 4"
 
-	var city [4]string
-	city[0] = "City One"
-	city[1] = "City Two"
-	city[2] = "City Three"
-	city[3] = "City Four"
+	//fmt.Println(city)
+	fmt.Println(path1)
+	fmt.Println(complie)
+	fmt.Println(aCity)
+	fmt.Println(section)
 
-	fmt.Println(city)
 
-	fmt.Println("You took ", path[PathInt], " to get to ", city[CityInt])
 
-	fmt.Println(path)
+	fmt.Println("-------------")
+	var new = complie[0]
+	fmt.Println(new[1][0])
 }
