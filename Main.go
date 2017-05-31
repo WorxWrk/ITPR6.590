fd21a18345b18b05507c97c8d6ce06c1e4cef3e5
Cameron <cameron-a-1997@hotmail.com>
2017-06-01 02:10:36 +1200

    Main part of the program is completed. Current fixes required are: Exit/Panic when the input is not Int. Remove the comments, make the code readable. Print current mass prints the array with the starting and ending [], try to use a loop for the array

--- Main.go
+++ Main.go
@@ -5,6 +5,8 @@ import (
 	"bufio"
 	"os"
 	"math/rand"
+	"strings"
+	"strconv"
 
 )
 
@@ -36,22 +38,31 @@ import (
 	// --------------------------------------------- Function Creation
 	// Creates the functions
 
-func startingCity(driver Driver) driver{
+func startingCity(driver Driver, allCities [4]City) Driver{
 
-	return
+	driver.currentCityNumber = driver.newNumber
+	driver.currentCity = allCities[driver.currentCityNumber]
+	driver.currentCityName = driver.currentCity.cityName
+
+	return driver
 }
 
-func count(driver Driver) driver{
+func count(driver Driver) Driver{
 
+	if driver.currentCityName == "Akina" {
+			driver.akinaCount = driver.akinaCount + 1
+	}
 
-	return
+	return driver
 }
 
 func driveCity(driver Driver, allCities [4]City) Driver {
 
-	driver.currentCityName = driver.
+	driver.currentCityName = driver.currentCity.cityName
 
 	if driver.newNumber == 1 { // right side path, int goes up
+		driver.pathName = driver.currentCity.path2
+		driver.exitName = driver.currentCity.exit2
 		if driver.currentCityNumber == 3 { // restricts int size to keep it inside the allCities array
 			driver.currentCityNumber = 0
 		} else {
@@ -60,6 +71,8 @@ func driveCity(driver Driver, allCities [4]City) Driver {
 	}
 
 	if driver.newNumber == 0{ // left side path, int goes down
+		driver.pathName = driver.currentCity.path1
+		driver.exitName = driver.currentCity.exit1
 		if driver.currentCityNumber == 0 { // restricts int size to keep it inside the allCities array
 			driver.currentCityNumber = 3
 		} else {
@@ -68,28 +81,65 @@ func driveCity(driver Driver, allCities [4]City) Driver {
 	}
 
 	driver.currentCity = allCities[driver.currentCityNumber]
-	allCities.pathName = driver.currentCity.pathName
-	allCities.exitName = driver.currentCity.exitName
 	driver.nextCityName = driver.currentCity.cityName
 
 // Return the driver type with updated values
 	return driver
 }
 
-func driveExit(driver Driver) driver{
+func driveExit(driver Driver) Driver{
 
 	if driver.newNumber == 2 { //  number that allows the driver to exit
 		driver.key = true
+		driver.nextCityName = "Outside City"
 	}
 // Return the driver type with updated values
 	return driver
 }
 
-func extras(driver Driver) driver{
+func extras(driver Driver) Driver{
 
+	var strDriverNumber string
+	strDriverNumber = strconv.Itoa(driver.driverNumber)
 
-	return
+	var strAkinaCount string
+	strAkinaCount = strconv.Itoa(driver.akinaCount)
+
+	mergedStr := strings.Join([]string{"\nDriver", strDriverNumber, "Met With Jhon Jamieson", strAkinaCount, "Times!"}, " ")
+	driver.totalOutput = append(driver.totalOutput, mergedStr)
+
+	if driver.akinaCount != 1 | 2 {
+		if driver.akinaCount == 0{
+			driver.totalOutput = append(driver.totalOutput, "\nThat Driver Missed Out!")
+		}
+		if driver.akinaCount <= 3{
+			driver.totalOutput = append(driver.totalOutput, "\nThat Driver Needed Lots Of Help!")
+		}
+	}
+	fmt.Println("-----")
+	return driver
 }
+
+func driveCityPrint (driver Driver) Driver{
+
+	var strDriverNumber string
+	strDriverNumber = strconv.Itoa(driver.driverNumber)
+
+	mergedStr := strings.Join([]string{
+		"\nDriver",
+		strDriverNumber,
+		"heading from",
+		driver.currentCityName,
+		"to",
+		driver.nextCityName,
+		"via",
+		driver.pathName },
+		 " ")
+	driver.totalOutput = append(driver.totalOutput, mergedStr)
+
+	return driver
+}
+
 
 /*
 func simulation () {
@@ -182,20 +232,16 @@ func main() {
 // Looking back. Should have used a struct instead of the 2-D arrays
 
 // City 1
-	City1 := City {"Exit 1","Path 1","City","Exit 2","Path2"}
-	var City1 [3][2] string
+	City1 := City {"Path 1","Exit 1","City1","Path2","Exit 2"}
 
 //City 2
-	City2 := City {"Exit 1","Path 1","City","Exit 2","Path2"}
-	var City2 [3][2] string
+	City2 := City {"Path 1","Exit 1","City2","Path2","Exit 2"}
 
 //City 3
-	City3 := City {"Exit 1","Path 1","City","Exit 2","Path2"}
-	var City3 [3][2] string
+	City3 := City {"Path 1","Exit 1","Akina","Path2","Exit 2"}
 
 //City 4
-	City4 := City {"Exit 1","Path 1","City","Exit 2","Path2"}
-	var City4 [3][2] string
+	City4 := City {"Path 1","Exit 1","City4","Path2","Exit 2"}
 
 // Array Creation - Create a single array to hold the previous 4 arrays.
 // Array for holding all city sections/quadrants.
@@ -209,7 +255,7 @@ func main() {
 // Variables will be initialized later, during the loops
 
 // Used to keep a driver in a loop
-	var key bool = false;
+	//var key bool = false;
 
 // Used to hold the Number given by the Random number generator
 	//var pathSelection int
@@ -218,35 +264,35 @@ func main() {
 	//var exitSelection int
 
 // Used to hold the Number given by the Random number generator
-	var newNumber int
+	//var newNumber int
 
 //Holds the conveted input from the user
 	var seed int
 
 // Holds the number of the current city, within the "allCities" array.
 //Used to set the "currentCity" variable
-	var currentCityNumber int
+	//var currentCityNumber int
 
 // Used to hold an entire City Section
-	var currentCity [3][2]string
+	//var currentCity [3][2]string
 
 // Used to track the amount of times the driver has visted "Akina"
-	var akinaCount int
+	//var akinaCount int
 
 // Stores the name of the City the driver is in before the driver moves to the next city
-	var currentCityName string
+	//var currentCityName string
 
 // Used to hold the current driver. Changes depending on the loop number
-	var driverNumber int = 0;
+	//var driverNumber int = 0;
 
 // Holds the string value of the path that has been selected
-	var pathName string
+	//var pathName string
 
 // Holds the string value of the exit that has been selected
-	var exitName string
+	//var exitName string
 
 // Holds name of the city that has been selected next
-	var nextCityName string
+	//var nextCityName string
 
 
 
@@ -258,13 +304,17 @@ func main() {
 	fmt.Println("Please enter a number:");
 	scanner.Scan()
 	input := scanner.Text()
+	seed, err := strconv.Atoi(input)
 
-	seed = /*input*/ 5
+	for userKey := false ; userKey == false{
+		if err != nil{
+			fmt.Println("Please provide an input of the Integer type")
+		}
 
-	rand.Seed(seed)
+	rand.Seed(int64(seed))
 
 // Loop to complete multiiple driving instances
-	for i:=0; i>5; i++{
+	for i:=0; i<5; i++{
 
 // Varible re-Initialization to start a driving instance
 
@@ -272,18 +322,20 @@ func main() {
 			var totalOutput []string
 
 // Varable for the new type "Driver".
-			driver := Driver 	{ false, 0, 0, 0, City1, "No City", "No Path", "No Exit", "No City", ""}
+			driver := Driver { i+1, false, 0, 0, 0, City1, "No City", "No Path", "No Exit", "No City", totalOutput}
 
 // Finding a Starting City for the driving instance
 
-			startingCity();
+			driver.newNumber = rand.Intn(4)
+			driver = startingCity(driver, allCities);
 
 // Loop for the moving sections of a single driving instance
-			for key == false{
+			for driver.key == false{
 
 // Path Decision. What path the driver will take out of the current city.
 				//set rnd Variable
 				driver.newNumber = rand.Intn(2)
+				driver = count(driver)
 
 				driver = driveCity(driver, allCities);
 
@@ -293,21 +345,25 @@ func main() {
 //Exit Decision. If the Driver will use an exit or continue to the city.
 				// new rnd
 				// set RND varablies
-				newNumber = rand.Intn(6)
+				driver.newNumber = rand.Intn(6)
 
-				driveExit();
+				driver = driveExit(driver);
+				driver = driveCityPrint(driver);
 				// printing stuff (just print Or move the print text to a variable to be printed later)
 			}
 
 // Additional Dialouge. The exta line required at the end of a driving instance.
 
-				extras();
+				driver = extras(driver);
+
+// Print the all the output from this drive instance
+				//for
+				fmt.Println(driver.totalOutput)
 	}
 
 
 
-
-						fmt.Println(seed,currentCity,akinaCount,currentCityName,pathName,exitName,nextCityName,input)
+						//fmt.Println(seed,currentCity,akinaCount,currentCityName,pathName,exitName,nextCityName,input)
 
 /*
 
@@ -354,4 +410,7 @@ if input2 == 1 { // will need to keep the varialbes above for handling the names
 	fmt.Println(new[1][0])
 
 	*/
+
+
+
 }
